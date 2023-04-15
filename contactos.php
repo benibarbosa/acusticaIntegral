9741329814df7b50e5436367a0bd4c46e44ccd8f
Pruebas0183 <34724846+Pruebas0183@users.noreply.github.com>
2023-04-15 12:07:56 -0600

    Update contactos.php

--- contactos.php
+++ contactos.php
@@ -63,7 +63,7 @@
 <script>
 window.open("Whatsapp","_blank");
 location.href= "https://api.whatsapp.com/send?phone=5215545055919&text=Hola%2C%20me%20interesar%C3%ADa%20tener%20m%C3%A1s%20informacion";
-</script>';
+</script>'
     }
 
 ?>
